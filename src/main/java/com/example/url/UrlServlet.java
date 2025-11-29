3b938780edab4558c208db0ddc2bb901b3ef40de
RKC906 <kevincedric59@gmail.com>
2025-11-29 20:09:45 +0300

    Update UrlServlet.java

--- src/main/java/com/example/url/UrlServlet.java
+++ src/main/java/com/example/url/UrlServlet.java
@@ -127,32 +127,6 @@ public class UrlServlet extends HttpServlet {
         }
     }
 
-   
-    //  Injection automatique des paramètres
-    private Object[] injectParams(HttpServletRequest req, Method method) {
-
-        Parameter[] parameters = method.getParameters();
-        Object[] values = new Object[parameters.length];
-
-        Map<String, String> pathParams = (Map<String, String>) req.getAttribute("pathParams");
-
-        for (int i = 0; i < parameters.length; i++) {
-            String name = parameters[i].getName();
-
-            // paramètres dynamiques (ex: /user/{id})
-            if (pathParams != null && pathParams.containsKey(name)) {
-                values[i] = convertType(pathParams.get(name), parameters[i].getType());
-                continue;
-            }
-
-            // query string et forms
-            String value = req.getParameter(name);
-            values[i] = convertType(value, parameters[i].getType());
-        }
-
-        return values;
-    }
-
     // MATCHING URL DYNAMIQUE : /user/{id}
     private Method matchDynamicRoute(String path, HttpServletRequest req) {
 
